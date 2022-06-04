295f6827c2b654bd130b5e143751690866e3984d
tao <changlin_zhao@qq.com>
2022-06-04 17:45:08 +0800

    add qr-code

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -90,6 +90,18 @@ class ComposerStaticInit1b32198725235c8d6500c87262ef30c2
         array (
             'Firebase\\JWT\\' => 13,
         ),
+        'E' => 
+        array (
+            'Endroid\\QrCode\\' => 15,
+        ),
+        'D' => 
+        array (
+            'DASPRiD\\Enum\\' => 13,
+        ),
+        'B' => 
+        array (
+            'BaconQrCode\\' => 12,
+        ),
         'A' => 
         array (
             'Alipay\\EasySDK\\' => 15,
@@ -236,6 +248,18 @@ class ComposerStaticInit1b32198725235c8d6500c87262ef30c2
         'Firebase\\JWT\\' => 
         array (
             0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
+        ),
+        'Endroid\\QrCode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/endroid/qr-code/src',
+        ),
+        'DASPRiD\\Enum\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/dasprid/enum/src',
+        ),
+        'BaconQrCode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/bacon/bacon-qr-code/src',
         ),
         'Alipay\\EasySDK\\' => 
         array (
